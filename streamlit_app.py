b5db17c1bf83ff5c194932b37f14eeea279d995b
moghani <51992878+moghani@users.noreply.github.com>
2022-12-16 10:18:54 +1100

    Update streamlit_app.py
    Adds item list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My parents healthy diner !!")
 streamlit.header('Breakfast Menu')
@@ -9,3 +10,5 @@ streamlit.text('🥑🍞Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+item_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(item_list)
